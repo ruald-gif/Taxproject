cc9bf20748b502153d3e44d9aa4ce63f5da3dedd
ruald <ruald19842006@gmail.com>
2026-02-08 20:12:15 +0300

    Add space point's

--- Tax/ConsoleApp1/Program.cs
+++ Tax/ConsoleApp1/Program.cs
@@ -1,2 +1,29 @@
-﻿
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }      
+    }
+    public struct Point
+    {
+        public int X { get; }
+        public int Y { get; }
+
+        public Point(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+        public double DistanceTo(Point orderPoint)//orderPoint - точка заказа
+        {
+            double dx = X - orderPoint.X;
+            double dy = Y - orderPoint.Y;
+            return Math.Sqrt(dx * dx + dy * dy);
+        }
+    }
+}
