381eee0aebdd97e1980a11bf66cd89d397777e7f
ruald <ruald19842006@gmail.com>
2026-02-08 21:13:15 +0300

    Add simplefinder

--- Tax/ConsoleApp1/Program.cs
+++ Tax/ConsoleApp1/Program.cs
@@ -19,7 +19,7 @@ namespace ConsoleApp1
             X = x;
             Y = y;
         }
-        public double DistanceTo(Point orderPoint)//orderPoint - точка заказа
+        public double DistanceTo(Point orderPoint)// orderPoint - точка заказа
         {
             double dx = X - orderPoint.X;
             double dy = Y - orderPoint.Y;
@@ -28,7 +28,7 @@ namespace ConsoleApp1
     }
     public class Driver
     {
-        public string Id { get; }//уникальный ID водителя
+        public string Id { get; }// уникальный ID водителя
         public Point LocationDriver { get; set; }
         public Driver(string id, Point location)
         {
@@ -47,4 +47,37 @@ namespace ConsoleApp1
 
         string AlgorithmName { get; }
     }
+
+    public class SimpleFinder:IDriverFinder
+    {
+        public string AlgorithmName => "SimpleFinder";
+        public List<Driver> FindNearestDrivers(List<Driver> drivers, Point orderLocation, int count = 5)
+        {
+            // Если водителей меньше или равно нужному количеству - возвращаем всех
+            if (drivers.Count <= count)
+                return new List<Driver>(drivers);
+
+            // 1. Создаем список пар "водитель-расстояние"
+            var driversWithDistance = new List<(Driver Driver, double Distance)>();
+
+            foreach (var driver in drivers)
+            {
+                double distance = driver.GetDistanceTo(orderLocation);
+                driversWithDistance.Add((driver, distance));
+            }
+
+            // 2. Сортируем по расстоянию (от ближнего к дальнему)
+            driversWithDistance.Sort((a, b) => a.Distance.CompareTo(b.Distance));
+
+            // 3. Берем первых count водителей
+            var result = new List<Driver>();
+            for (int i = 0; i < count; i++)
+            {
+                result.Add(driversWithDistance[i].Driver);
+            }
+
+            return result;
+        }
+    }
+
 }
